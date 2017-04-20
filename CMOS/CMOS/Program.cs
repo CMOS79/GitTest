bc0b110a2ca95eff7f312c8b5fc4add7f9ff9ab5
Christian Moser <christian.moser@melecs.com>
2017-04-20 15:28:37 +0200

    MOS79

---
+++ CMOS/CMOS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
